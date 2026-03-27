231cf640407700f87e7534b867cc8c54e48bb31d
jobiyax <josuebiyayi404@gmail.com>
2026-03-27 22:59:35 +0100

    chore: add bubbletea

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct{}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg.(type) {
+	case tea.KeyMsg:
+		return m, tea.Quit
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return "Dock Lift CLI\n\n(Appuyez sur une touche pour quitter)\n"
+}
 
 func main() {
-	fmt.Println("Dock Lift CLI")
+	p := tea.NewProgram(model{})
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Erreur:", err)
+		os.Exit(1)
+	}
 }
